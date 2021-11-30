3b81613841029eb5eb4820932c7185c59d4940c2
sabazshereef <sabazshereef91@gmail.com>
2021-11-30 16:54:15 +0530

    Api Call and Model set up

---
+++ PlayoCodingTest/Headlines/HeadlinesModel.swift
@@ -0,0 +1,44 @@
+//
+//  HeadlinesModel.swift
+//  PlayoCodingTest
+//
+//  Created by sabaz shereef on 30/11/21.
+//
+
+import Foundation
+
+// MARK: - Headlines
+struct Headlines: Codable {
+    let status: String
+   let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let author, title, articleDescription: String
+    let url: String
+    let urlToImage: String
+    let content: String
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: ID
+    let name: Name
+}
+
+enum ID: String, Codable {
+    case techcrunch = "techcrunch"
+}
+
+enum Name: String, Codable {
+    case techCrunch = "TechCrunch"
+}
---
+++ PlayoCodingTest/Headlines/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  PlayoCodingTest
+//
+//  Created by sabaz shereef on 30/11/21.
+//
+
+import Foundation
+
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    enum errors : Error {
+        case InvalidUrl
+        case NoData
+        
+    }
+    
+    func getHedalinesData <T: Codable> (urlString: String, expecting: T.Type, completion: @escaping(Result<T,Error>) -> Void){
+        
+        
+        guard let url = URL(string: urlString) else{
+            completion(.failure(errors.InvalidUrl))
+            return
+        }
+    
+        let dataTask = URLSession.shared.dataTask(with: url) { data,_,error in
+            guard let jsonData = data else{
+                completion(.failure(errors.NoData))
+                return
+            }
+           
+            do {
+                
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(expecting, from: jsonData)
+                completion(.success(result))
+            }
+            catch{
+                completion(.failure(error))
+            }
+        };
+        dataTask.resume()
+
+       
+        
+    }
+}
